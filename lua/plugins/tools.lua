dfbe70412db28bd591ba2a691fef4594165efdd5
khiz125 <ken.hiro118@gmail.com>
2026-07-24 14:45:08 +0900

    update

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -2,8 +2,12 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
       formatters_by_ft = {
-        python = { "ruff_format" },
+        python = { "ruff_fix", "ruff_format" },
         lua = { "stylua" },
       },
     },
